84a95cea1a1d49b129e5d0d39a8f5526f61c6b03
Subhanshu Bansal <subhanshu20135@iiitd.ac.in>
2023-12-21 01:04:40 +0530

    Initialised HTTP Server and Added Socket.io Dependency

---
+++ apps/server/src/index.ts
@@ -0,0 +1,15 @@
+import http from 'http';
+
+async function init() {
+    const httpServer = http.createServer();
+    const PORT = process.env.PORT ? process.env.PORT: 8000;
+
+    httpServer.listen(
+        PORT,
+        () => console.log(
+            `HTTP Server Started at ${PORT}`
+        )
+    );
+}
+
+init();
